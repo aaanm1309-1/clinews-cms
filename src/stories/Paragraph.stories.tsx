3291b8d433be0ccfbff25b74463d00acfb2aac5a
aaanm13 <aaanm1309.1@gmail.com>
2021-07-08 13:13:09 -0300

    Aula 4.15

---
+++ src/stories/Paragraph.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import  Paragraph, { ParagraphProps } from '../components/Typography/Paragraph';
+
+export default {
+  title: 'Typography/Paragraph',
+  component: Paragraph,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Paragraph>;
+
+const Template: ComponentStory<typeof Paragraph> = (args) => <Paragraph {...args} />;
+
+
+export const Default = Template.bind({});
+
+Default.args = {
+  size: 'default' ,
+  children: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. '
+}
+
+export const Small = Template.bind({});
+
+Small.args = {
+  size: 'small' ,
+  children: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. '
+}
